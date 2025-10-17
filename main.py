569c48a2ed61f1dcb7ae306e738eaa9b09fcf106
zzzpz <1911293650@qq.com>
2025-10-17 12:44:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 input_str = input()
-letters = 0
-digits = 0
-spaces = 0
-others = 0
+
+letter_count = 0
+digit_count = 0
+space_count = 0
+other_count = 0
+
 for char in input_str:
-    if 'a' <= char <= 'z' or 'A' <= char <= 'Z':
-        letters += 1
+    if char.isalpha():
+        letter_count += 1
     elif char.isdigit():
-        digits += 1
+        digit_count += 1
     elif char.isspace():
-        spaces += 1
+        space_count += 1
     else:
-        others += 1
-print(f"英文字符: {letters}")
-print(f"数字: {digits}")
-print(f"空格: {spaces}")
-print(f"其他字符: {others}")
+        other_count += 1
+
+print(f"英文字符: {letter_count}")
+print(f"数字: {digit_count}")
+print(f"空格: {space_count}")
+print(f"其他字符: {other_count}")
