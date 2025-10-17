5577f9ac9a88c3b89165ca17ccdff31456b8004d
zzzpz <1911293650@qq.com>
2025-10-17 11:38:27 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-# 这个文件用于编写代码
+input_str = input()
+
+letter_count = 0
+digit_count = 0
+space_count = 0
+other_count = 0
+
+for char in input_str:
+    if char.isalpha():
+        letter_count += 1
+    elif char.isdigit():
+        digit_count += 1
+    elif char.isspace():
+        space_count += 1
+    else:
+        other_count += 1
+
+print(f"英文字符: {letter_count}")
+print(f"数字: {digit_count}")
+print(f"空格: {space_count}")
+print(f"其他字符: {other_count}")
